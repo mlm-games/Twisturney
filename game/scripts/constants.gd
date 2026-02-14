4c2208ac81abf864948c257101b7dc6f422c877b
ragebreaker <125530737+mlm-games@users.noreply.github.com>
2026-02-14 08:32:17 +0530

    Initial commit

---
+++ game/scripts/constants.gd
@@ -0,0 +1,24 @@
+class_name C extends Node # Constants
+
+enum BusNames { # Use get string from enum fn
+	Master,
+	Music,
+	Sfx,
+}
+
+const SCREENS = {
+	CREDITS = "uid://bq0gelfcjnqvg",
+	SETTINGS = "uid://dp42fom7cc3n0",
+	MENU = "uid://ddl5roo03rvdl",
+	END = "uid://o7bist5hmyv6",
+}
+
+const RESOURCES = {
+	SHADERS = {
+		#CIRCULAR_ENGULF = preload("uid://dnms5hsmipkyv")
+	}
+}
+
+const PATHS =  {
+	
+}
